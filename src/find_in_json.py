39f4839fd5d1b5d4bc2dcf23f8da7a6cd82b885e
Parth Sarangi <parth.sar@ascendcorp.com>
2019-07-17 14:03:11 +0700

    added new scripts

---
+++ src/find_in_json.py
@@ -0,0 +1,13 @@
+# import os
+# import json
+import pandas as pd
+
+def read_json(dir):
+    # file1 = open(dir+'tr_order_20190704_000915_1.csv', 'r')
+    file1 = pd.read_csv(dir + 'tr_order_20190704_000915_1.csv')
+    if_valu = file1['order_id']=='19070323092898061140500'
+    print(if_valu.head())
+
+if __name__ == '__main__':
+    path = '/Users/parth/Documents/work/vn/payment_tr_order/csv/0704/'
+    read_json(path)
